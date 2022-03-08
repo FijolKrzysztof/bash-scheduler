3ea10ab9e77d843c7072ee4afdc557582e652058
FijolKrzysztof <xkrzysztoffijol@gmail.com>
2022-03-08 16:47:11 +0100

    commit

---
+++ open-website.py
@@ -0,0 +1,28 @@
+#!/home/ja/anaconda3/envs/python/bin/python
+
+import webbrowser
+import socket
+import time
+import sys
+
+
+def connected():
+    try:
+        socket.create_connection(("1.1.1.1", 53))
+        return True
+    except OSError:
+        pass
+    return False
+
+
+t = 0
+x = 0
+
+while x == 0:
+    if connected() is True:
+        x = 1
+        webbrowser.open_new(sys.argv[1])
+    time.sleep(1)
+    t = t+1
+    if t == 10:
+        x = 1
--- scheduler.data.sh
+++ scheduler.data.sh
@@ -3,7 +3,7 @@
 displayTime=22:00
 
 openBetting="node /home/ja/programs/betting/server/index.js"
-openWebsite="/home/ja/programs/open-website/open-website.py"
+openWebsite="/home/ja/programs/bash-scheduler/open-website.py"
 
 calendarURL="https://docs.google.com/spreadsheets/d/1MI6fjTXBgaJCA8vLf3MvMY8NvxmQdBdefi1smyybQ6s/edit#gid=888205070"
 btcURL="https://www.google.com/search?q=btc%2Fusd"
