6f7d278203111259419446a06303296352a2fcde
FijolKrzysztof <xkrzysztoffijol@gmail.com>
2023-10-15 23:14:59 +0200

    Poprawa skryptów

--- scheduler-data.sh
+++ scheduler-data.sh
@@ -4,6 +4,6 @@ displayTime=00:00
 
 openWebsite="/home/krzysztof/Apps/bash-scheduler/open-website.py"
 
-calendarURL="https://docs.google.com/spreadsheets/d/1MI6fjTXBgaJCA8vLf3MvMY8NvxmQdBdefi1smyybQ6s/edit#gid=888205070"
+calendarURL="https://docs.google.com/spreadsheets/d/10pRjMtX-4UpwNRVFNly9oePMwvPmVR6XLS-42uEe5pU/edit#gid=888205070"
 btcURL="https://www.google.com/search?q=btc%2Fusd"
 chartURL="https://studio.glassnode.com/metrics?a=BTC&category=&m=supply.ActiveMore1YPercent"
--- scheduler-exec.sh
+++ scheduler-exec.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-source scheduler-data.sh
+source /home/krzysztof/Apps/bash-scheduler/scheduler-data.sh
 read -r -p "OPEN( HABITS | BTC | CHART )? [h b c] : " habits btc chart
 if [ $habits ] && [ $habits == "h" ]; then
-	command $openWebsite $calendarURL
+	command $openWebsite $calendarURL > /dev/null 2>&1 &
 fi
 if [ $btc ] && [ $btc == "b" ]; then
-	command $openWebsite $btcURL
+	command $openWebsite $btcURL > /dev/null 2>&1 &
 fi
 if [ $chart ] && [ $chart == "c" ]; then
-	command $openWebsite $chartURL
+	command $openWebsite $chartURL > /dev/null 2>&1 &
 fi
 read -p "REOPEN [y/n] : " reopen
 if [ $reopen ] && [ $reopen == "y" ]; then
-	source scheduler-exec.sh
+	source /home/krzysztof/Apps/bash-scheduler/scheduler-exec.sh
 fi
